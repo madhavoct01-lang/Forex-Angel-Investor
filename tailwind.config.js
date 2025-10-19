6fc4a89f8a85b9745b4af2176fc9805e4bce2f1f
madhavoct01-lang <madhavoct01@gmail.com>
2025-10-20 02:16:05 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        navy: "#0B0C3A",
+        gold: "#F5B23E",
+        light: "#EAEAEA",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+
+
